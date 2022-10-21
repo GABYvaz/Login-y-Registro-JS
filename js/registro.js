5bf52ad1e97711d5f5d964f6142fbf2d989d9a9c
Gaby Vazquez <gaby27anahi02@outlook.es>
2022-10-21 02:18:44 -0500

    validando login

--- js/registro.js
+++ js/registro.js
@@ -36,15 +36,3 @@ function registro(e){
 
     creaMensaje('Usuario Registrado!','seccess');
 }
-
-function creaMensaje(texto, tipo){
-    const nuevoElemento = document.createElement('div');
-    nuevoElemento.innerText = texto;
-    nuevoElemento.classList.add('alert','alert-' + tipo);
-    const divMensaje = document.getElementById('mensaje');
-    divMensaje.appendChild(nuevoElemento);
-    setTimeout(function(){
-        nuevoElemento.remove();
-    },2000);
-
-}
